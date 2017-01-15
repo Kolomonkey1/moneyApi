5840dcd988998e65d6de63ac689afeb74106ce83
Jose <jperez@redventures.com>
2017-01-14 20:03:27 -0500

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-// imports all the files you will be using
-// when you use a libary you do not have to specify the path
-// when using a file you created you do have to specify the path
 import _ from 'lodash';
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
 import SymbolDetail from './symbol_detail';
 
-
-// this is the top level component to which we will be passing in all the other components
 class App extends Component {
-// the constructor will be recognized by react and will run first
-// here we are setting the initial state
   constructor(props) {
     super(props);
 
     this.state = { 
    };
-// changing the passed in param here will set the inital search for the videos   
    this.symbolSearch('MSFT');
   }
-// this function uses YTSearch from imports
-// to make the api call for the videos
+
   symbolSearch(term) {
     const url = `https://api.robinhood.com/quotes/?symbols=${term}&token=9e69c3b5306b5653ae1da50ac07dc2a42e9793ea`;
     const myHeaders = new Headers();
@@ -42,11 +33,9 @@ class App extends Component {
     })
   }
   render() { 
-/* this uses lodash so the video search does 
-   not reload the page on every key stroke, it has a delay
-   of how ever much time you pass in as the second argument */
+
     const symbolSearch = _.debounce((term) => { this.symbolSearch(term) }, 500);
-// in the return you pass in the child components you want to render
+    
     return (
       <div>
         <SearchBar onSearchTermChange={symbolSearch} />
--- src/symbol_detail.js
+++ src/symbol_detail.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
-// here we pass in the video object that comes 
-// from the top level component so we can have access to it
 const SymbolDetail = ({symbol}) => {
-  // if video is not populated then it will should a loading message.
+
   if(!symbol) {
     return <div>Loading...</div>;
   }
- 
 
-// returns the video and information using the video object that was passed in
-// iframe allows you to play the video on the page
   return (
     <div>
       <div className="details">
@@ -45,5 +40,4 @@ const SymbolDetail = ({symbol}) => {
   );
 };
 
-// you export so you can have access to this component in your other files
 export default SymbolDetail;
